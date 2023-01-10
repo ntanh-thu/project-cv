9e8dc4e63807b6e42a87abdf6dfad6bc172c57f4
thu.nguyen <94220054+ntanh-thu@users.noreply.github.com>
2023-01-10 14:58:56 +0700

    Done footer UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { React } from 'react';
 import Header from './components/Layout/Header';
 import './App.css';
+import Footer from './components/Layout/Footer';
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <Footer></Footer>
         </div>
     );
 }
