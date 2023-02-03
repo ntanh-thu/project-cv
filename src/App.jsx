15e10497024549e27a781ca7ed8e923473bd5644
thu.nguyen <94220054+ntanh-thu@users.noreply.github.com>
2023-02-03 14:47:00 +0700

    Add cv form page

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,13 @@ import Header from './components/Layout/Header';
 import Footer from './components/Layout/Footer';
 import './App.css';
 import './sass/index.scss';
+import CvForm from './pages/CvForm';
 
 function App() {
     return (
         <div className="App">
             <Header></Header>
+            <CvForm></CvForm>
             <Footer></Footer>
         </div>
     );
